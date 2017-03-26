4e079176d0a03caedd1d99db9432c896f06bd7a0
edwinchen85 <edwinchen85@gmail.com>
2017-03-26 17:53:42 +0800

    Write gulp sass task. Run and compile scss to css file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('hello', function() {
   console.log('Hello Edwin');
+});
+
+gulp.task('sass', function() {
+  gulp.src('app/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'));
 });

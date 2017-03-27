cd8fe7cfabe1eb75ddabbeb2d5ef37576baa8c5d
edwinchen85 <edwinchen85@gmail.com>
2017-03-27 09:08:58 +0800

    Create browserSync task with basic configuration. Include this task as dependency for watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,29 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
   gulp.src('app/scss/**/*.scss')
     .pipe(customPlumber('Error Running Sass'))
     .pipe(sass())
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/css'))
+    // Tells Browser Sync to reload files when task is done
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', ['browserSync', 'sass'], function() {
   gulp.watch('app.scss/**/*.scss', ['sass']);
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
 });
 
 function customPlumber(errTitle) {
